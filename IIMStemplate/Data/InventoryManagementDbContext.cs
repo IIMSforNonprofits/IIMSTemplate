35b6aa25129d16481fe2a83d0d797b1fdb608d98
Jimmy Chang <jimmyfchang94@gmail.com>
2018-12-03 17:23:31 -0800

    Adds DbContext for data and sets up corresponding models (rows not complete)

---
+++ IIMStemplate/Data/InventoryManagementDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IIMStemplate.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IIMStemplate.Data
+{
+    public class InventoryManagementDbContext : DbContext
+    {
+        public InventoryManagementDbContext(DbContextOptions<InventoryManagementDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<Log> Logs { get; set; }
+        //public DbSet<Donor> Donors { get; set; }
+    }
+}
