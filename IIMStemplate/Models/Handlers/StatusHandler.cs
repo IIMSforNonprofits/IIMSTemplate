09614bf8a3b7d91a6ae48ef8ef691fb9fb474297
Jimmy Chang <jimmyfchang94@gmail.com>
2018-12-03 17:25:28 -0800

    Sets up status policy and viewimports

---
+++ IIMStemplate/Models/Handlers/StatusHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IIMStemplate.Models.Handlers
+{
+    public class StatusHandler : AuthorizationHandler<StatusRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, StatusRequirement requirement)
+        {
+            if(!context.User.HasClaim(c => c.Type == "Status"))
+            {
+                return Task.CompletedTask;
+            }
+
+            var userStatus = context.User.FindFirst(c => c.Type == "Status").Value;
+
+            //if(userStatus == requirement.Status)
+            //{
+            //    context.Succeed(requirement);
+            //}
+
+            return Task.CompletedTask;
+        }
+    }
+}
