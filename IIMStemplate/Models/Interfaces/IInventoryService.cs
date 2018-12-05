9aedf5ca9aa8987c4e9a54015bc460d91f6e5360
Eric Singleton <eric.singleton.jr@gmail.com>
2018-12-04 19:57:27 -0800

    Started IInventoryService method signatures

---
+++ IIMStemplate/Models/Interfaces/IInventoryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IIMStemplate.Models.Interfaces
+{
+    /// <summary>
+    /// This interface defines our required methods to interact with the inventory
+    /// database with relation to products and orders
+    /// </summary>
+    interface IInventoryService
+    {
+        //These method signatures define basic CRUD operations
+        //for product
+        Task<HttpStatusCode> CreateProduct(Product product);
+        Task<Product> GetProductByID(int id);
+        Task<Product> UpdateProduct(int id);
+        Task<HttpStatusCode> DeleteProduct(int id);
+    }
+}
