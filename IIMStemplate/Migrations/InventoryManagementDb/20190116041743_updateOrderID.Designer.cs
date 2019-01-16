0733bae4080c0ade1c4a45034e6d6376696abae7
Eric Singleton <eric.singleton.jr@gmail.com>
2019-01-15 20:35:02 -0800

    Started api controller for inventory

--- IIMStemplate/Migrations/InventoryManagementDb/20190116041743_updateOrderID.Designer.cs
+++ IIMStemplate/Migrations/InventoryManagementDb/20190116041743_updateOrderID.Designer.cs
@@ -4,14 +4,16 @@ using IIMStemplate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IIMStemplate.Migrations.InventoryManagementDb
 {
     [DbContext(typeof(InventoryManagementDbContext))]
-    partial class InventoryManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190116041743_updateOrderID")]
+    partial class updateOrderID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
