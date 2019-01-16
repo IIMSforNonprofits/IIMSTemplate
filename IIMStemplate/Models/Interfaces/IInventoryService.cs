cf5917cdd07c3ed23e10f00c9d10e271abf88b22
Jimmy Chang <jimmyfchang94@gmail.com>
2019-01-15 19:36:51 -0800

    Implementing methods for inventory service. Also updates the IInventoryService to have the correct method signatures

--- IIMStemplate/Models/Interfaces/IInventoryService.cs
+++ IIMStemplate/Models/Interfaces/IInventoryService.cs
@@ -11,19 +11,21 @@ namespace IIMStemplate.Models.Interfaces
     /// This interface defines our required methods to interact with the inventory
     /// database with relation to products and orders
     /// </summary>
-    interface IInventoryService
+    public interface IInventoryService
     {
         // These method signatures define CRUD operations
         // for product
+        Task<List<Product>> GetProducts();
         Task<HttpStatusCode> CreateProduct(Product product);
         Task<Product> GetProductByID(int id);
-        Task<Product> UpdateProduct(int id);
+        Task<HttpStatusCode> UpdateProduct(int id, Product product);
         Task<HttpStatusCode> DeleteProduct(int id);
         // These method signatures define the operations 
         // for orders
+        Task<List<Order>> GetOrders();
         Task<HttpStatusCode> CreateOrder(Order order);
         Task<Order> GetOrderByID(int id);
-        Task<Order> UpdateOrder(int id);
+        Task<HttpStatusCode> UpdateOrder(int id, Order order);
 
     }
 }
